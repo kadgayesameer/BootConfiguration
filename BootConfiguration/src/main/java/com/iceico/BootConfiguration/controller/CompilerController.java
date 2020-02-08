b408918506a2c15454146bc466e5d202f605f60e
Rajat Patil <patilrajat@805@gmail.com>
2020-02-08 19:14:08 +0530

    Responsive Triplets

--- BootConfiguration/src/main/java/com/iceico/BootConfiguration/controller/CompilerController.java
+++ BootConfiguration/src/main/java/com/iceico/BootConfiguration/controller/CompilerController.java
@@ -3,6 +3,14 @@
  */
 package com.iceico.BootConfiguration.controller;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.ParseException;
 import java.util.Locale;
 
@@ -10,7 +18,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.iceico.BootConfiguration.exceptions.ResourceNotFoundException;
 
@@ -29,7 +39,59 @@ public class CompilerController {
 	}
 
 	@RequestMapping("/admin/compiler")
-	public String categoryDashboard(ModelMap modelMap, Locale locale) throws ResourceNotFoundException, ParseException {
+	public String compilerDashboard(ModelMap modelMap, Locale locale) throws ResourceNotFoundException, ParseException {
+
+		modelMap.addAttribute("user", this.getPrincipal());
+		return "editor";
+	}
+
+	@PostMapping("/admin/compiler/run")
+	public String runCode(ModelMap modelMap, Locale locale, 
+			@RequestParam("code") String code, @RequestParam("language") String languageIn) throws ResourceNotFoundException, ParseException {		
+		
+		String clientId = "be9a209b50977cbd22c436977ab648d0"; //Replace with your client ID
+        String clientSecret = "55794f230a6a52e27542d1f034747eb869b7ceef03b5c8f9cb0ae726c4ee14cc"; //Replace with your client Secret
+        String script = code;
+        String language = languageIn;
+        String versionIndex = "2";
+        
+        try {
+            URL url = new URL("https://api.jdoodle.com/execute");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            String input = "{\"clientId\": \"" + clientId + "\",\"clientSecret\":\"" + clientSecret + "\",\"script\":\"" + script +
+            "\",\"language\":\"" + language + "\",\"versionIndex\":\"" + versionIndex + "\"} ";
+
+            System.out.println("input ===> "+input);
+            
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(input.getBytes());
+            outputStream.flush();
+
+//            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+//                throw new RuntimeException("Please check your inputs : HTTP error code : "+ connection.getResponseCode());
+//            }
+
+            BufferedReader bufferedReader;
+            bufferedReader = new BufferedReader(new InputStreamReader((connection.getInputStream())));
+
+            String output;
+            System.out.println("Output from JDoodle .... \n");
+            while ((output = bufferedReader.readLine()) != null) {
+                System.out.println("Output ====> "+output);
+            }
+
+            connection.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
 
 		modelMap.addAttribute("user", this.getPrincipal());
 		return "editor";
--- BootConfiguration/src/main/webapp/WEB-INF/demo/view/admin-module/compiler/editor.jsp
+++ BootConfiguration/src/main/webapp/WEB-INF/demo/view/admin-module/compiler/editor.jsp
@@ -14,37 +14,37 @@
 </head>
 <body>
 	<!--Compiler-->
-	<!-- 
-	<form id="editorForm" action="../controller/SaveController.php"
-		method="post"> -->
-	<div id="code-edit" class="row code-div container mx-auto">
-		<div id="editor-menu">
-			<select name="language" class="options" id="prolang">
-				<option value="java">Java</option>
-				<option value="python">Python</option>
-				<option value="c">C</option>
-				<option value="cpp">Cpp</option>
-				<option value="javascript">Javascript</option>
-			</select> <select class="options" id="theme">
-				<option value="dracula">Dark</option>
-				<option value="xcode">Light</option>
-			</select> <input type="text" hidden name="code" id="hiddencode">
-			<button id="screen">
-				<img id="screen-img"
-					src="${pageContext.request.contextPath }/static/img/compiler/short.png">
-			</button>
-			<button id="run" type="submit">
-				<img id="run-img"
-					src="${pageContext.request.contextPath }/static/img/compiler/run.png">
-			</button>
+	<form id="editorForm" action="${pageContext.request.contextPath }/admin/compiler/run"
+		method="post">
+		<div id="code-edit" class="row code-div container mx-auto">
+			<div id="editor-menu">
+				<select name="language" class="options" id="prolang">
+					<option value="java">Java</option>
+					<option value="python">Python</option>
+					<option value="c">C</option>
+					<option value="cpp">Cpp</option>
+					<option value="javascript">Javascript</option>
+				</select> <select class="options" id="theme">
+					<option value="dracula">Dark</option>
+					<option value="xcode">Light</option>
+				</select> <input type="text" hidden name="code" id="hiddencode">
+				<button id="screen">
+					<img id="screen-img"
+						src="${pageContext.request.contextPath }/static/img/compiler/short.png">
+				</button>
+				<button id="run" type="submit">
+					<img id="run-img"
+						src="${pageContext.request.contextPath }/static/img/compiler/run.png">
+				</button>
+			</div>
+			<div id="editor"></div>
+			<script
+				src="${pageContext.request.contextPath }/static/js/compiler.js"
+				type="text/javascript">
+				
+			</script>
 		</div>
-		<div id="editor"></div>
-		<script src="${pageContext.request.contextPath }/static/js/compiler.js"  type="text/javascript">
-		</script>
-	</div>
-	<!-- </form> -->
-
-
+	</form>
 </body>
 
 </html>
